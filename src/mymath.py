5340e514540814834ad607e8a81a0d1da42daa04
Park donghyeon <emma415g@gmail.com>
2023-07-15 18:21:53 +0900

    change gitignore

---
+++ src/mymath.py
@@ -0,0 +1,17 @@
+from typing import *
+
+
+def add(a: Union[int, float], b: Union[int, float]):
+    return a + b
+
+
+def subtract(a: Union[int, float], b: Union[int, float]):
+    return a - b
+
+
+def multiply(a: Union[int, float], b: Union[int, float]):
+    return a * b
+
+
+if __name__ == '__main__':
+    pass
---
+++ tests/test_math.py
@@ -0,0 +1,29 @@
+import unittest
+from src.mymath import *
+
+class SampleTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        print('setup class')
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        print('teardown class')
+    def setUp(self) -> None:
+        print('setup')
+
+    def tearDown(self) -> None:
+        print('teardown')
+
+    def test1_add(self):
+        r = add(10, 20)
+        self.assertEqual(r, 30, 'add testing')
+
+    # @unittest.skip('no reason')
+    def test2_add(self):
+        r = add(1, 2)
+        self.assertEqual(3, r)
+
+
+if __name__ == '__main__':
+    unittest.main()
